384f8faa6e3befbe4f0b34dd9163385befe6c1f3
shapikk <sashazet6@gmail.com>
2024-12-01 19:05:32 +0200

    add notify

--- storage.py
+++ storage.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+from plyer import notification
 
 REMINDERS_FILE = "./reminders.json"
 
@@ -18,8 +19,21 @@ def save_reminders(reminders):
         data = [{"text": text, "date": date.isoformat(), "status": status} for text, date, status in reminders]
         json.dump(data, output_file, indent=4)
 
+def show_notify(title, message):
+    notification.notify(
+        title=title,
+        message=message,
+        app_name='Reminder',
+        app_icon='',
+        timeout=5
+    )
+
 def reminder_updater(reminders):
     time_now = datetime.now()
     for index, (text, date, status) in enumerate(reminders):
         status = "past" if date < time_now else "active"
         reminders[index] = (text, date, status)
+
+        checkDateNotify = date - time_now
+        if status == "active" and 0 <= checkDateNotify.days < 1:
+            show_notify("Reminder for tomorrow!", f"Don't forget: {text}")
